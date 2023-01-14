97e0ece9569e187ac09ee60265a13618d6d71d30
SabnamJuhi <sjuhisarkars95@gmail.com>
2023-01-14 23:36:18 +0530

    Name changed

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Data = () => {
     return (
         <div className="home__data">
-            <h1 className="home__title">Hi, I'am Ashit Kumar Sinha</h1>
+            <h1 className="home__title">Hi, I'am Juhi Sabnam Sarkar</h1>
 
             <h3 className="home__subtitle">Full-Stack Developer</h3>
             <p className="home__description">
